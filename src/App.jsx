c216ee4673ff917169ecd2a46d368dc9cea9e69d
sedrlaP <prokop.sedrla@gmail.com>
2024-11-21 15:14:36 +0100

    Change paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
-import StartGameScreen from "./components/StartGameScreen"
-import EndGameScreen from "./components/EndGameScreen"
-import Timer from "./components/Timer"
+import StartGameScreen from "/src/components/StartGameScreen"
+import EndGameScreen from "/src/components/EndGameScreen"
+import Timer from "/src/components/Timer"
 
 const DEFAULT_NUMBER_OF_CARDS = 8
 
